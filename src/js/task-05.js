fedc047f01b2de273804986c4511b1f44814f81a
Anastasiia <114913935+staci-bog@users.noreply.github.com>
2023-03-02 21:04:31 +0200

    add changes

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,8 +1,8 @@
-//const ref = {
- //   input: document.querySelector('#name-input'),
- //   span: document.querySelector('#name-output'),
- // };
+const ref = {
+    input: document.querySelector('#name-input'),
+    span: document.querySelector('#name-output'),
+  };
   
-//  ref.input.addEventListener('input', event => {
-//    ref.span.textContent = event.currentTarget.value ? event.currentTarget.value : 'Anonymous';
-//  });
+  ref.input.addEventListener('input', event => {
+    ref.span.textContent = event.currentTarget.value ? event.currentTarget.value : 'Anonymous';
+  });
