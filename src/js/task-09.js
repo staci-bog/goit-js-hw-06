9dad233b0d41ae04c5c2bef2e86065e31310feb6
Anastasiia <114913935+staci-bog@users.noreply.github.com>
2023-03-02 21:26:27 +0200

    add info

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,6 +3,9 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+//Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color 
+//і виводить значення кольору в span.color.
+//Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
 //function getRandomHexColor() {
 //  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
