3041fe21b142804d764d2a84b92b9f670f6ad8ff
Anastasiia <114913935+staci-bog@users.noreply.github.com>
2023-03-02 21:14:16 +0200

    add changes

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,12 +1,12 @@
-//const ref = {
-    input: document.querySelector('#validation-input'),
-//  };
+const ref = {
+   input: document.querySelector('#validation-input'),
+  };
   
-//  ref.input.addEventListener('blur', event => {
- //   if (event.currentTarget.value.length !== parseInt(ref.input.getAttribute('data-length'))) {
- //     ref.input.classList.add('invalid');
- //   } else {
- //     ref.input.classList.remove('invalid');
-  //    ref.input.classList.add('valid');
- //   }
- // });
+   ref.input.addEventListener('blur', event => {
+    if (event.currentTarget.value.length !== parseInt(ref.input.getAttribute('data-length'))) {
+      ref.input.classList.add('invalid');
+    } else {
+      ref.input.classList.remove('invalid');
+      ref.input.classList.add('valid');
+    }
+   });
